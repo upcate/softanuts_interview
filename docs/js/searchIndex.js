dc4f897205522b82177bb01e87daede0bb39c8af
mswiatek1 <mil.swiatek1@gmail.com>
2022-07-05 10:19:58 +0000

    Configured static code analysis tools (php cs fixer, php code sniffer, psalm) and PhpDocumentor

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\App\\Controller\\GoldController",
+            "name": "GoldController",
+            "summary": "",
+            "url": "classes/App-Controller-GoldController.html"
+        },                {
+            "fqsen": "\\App\\Controller\\GoldController\u003A\u003Aindex\u0028\u0029",
+            "name": "index",
+            "summary": "",
+            "url": "classes/App-Controller-GoldController.html#method_index"
+        },                {
+            "fqsen": "\\App\\Kernel",
+            "name": "Kernel",
+            "summary": "",
+            "url": "classes/App-Kernel.html"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\App\\Controller",
+            "name": "Controller",
+            "summary": "",
+            "url": "namespaces/app-controller.html"
+        },                {
+            "fqsen": "\\App",
+            "name": "App",
+            "summary": "",
+            "url": "namespaces/app.html"
+        }            ]
+);
--- src/Controller/GoldController.php
+++ src/Controller/GoldController.php
@@ -8,13 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class GoldController extends AbstractController
 {
-    #[Route('/api/gold', name: 'app_gold', methods: ['POST'] )]
+    #[Route('/api/gold', name: 'app_gold', methods: ['POST'])]
     public function index(): JsonResponse
     {
         return $this->json([
             'from' => '2001-01-04T00:00:00+00:00',
             'to' => '2001-01-04T00:00:00+00:00',
-            'avg' => 228.1
+            'avg' => 228.1,
         ]);
     }
 }
