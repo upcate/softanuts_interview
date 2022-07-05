00cff8379e5ce2450ee9bf04a1ec1eb61fc8d53d
mswiatek1 <mil.swiatek1@gmail.com>
2022-07-05 09:44:46 +0000

    Initial set of files. Setting up symfony project.

---
+++ config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    HandcraftedInTheAlps\RestRoutingBundle\RestRoutingBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+];
